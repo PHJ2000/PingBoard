2a96c6355cbd796c26d6f4c1b0eda0750ce83eb9
Park Jae Hong <105151063+PHJ2000@users.noreply.github.com>
2026-03-14 16:08:58 +0900

    Fix Grafana datasource wiring and monitor metrics

--- src/main/java/com/pingboard/monitor/metrics/MonitorMetricsConfiguration.java
+++ src/main/java/com/pingboard/monitor/metrics/MonitorMetricsConfiguration.java
@@ -4,28 +4,36 @@ import com.pingboard.monitor.domain.MonitorStatus;
 import com.pingboard.monitor.repository.MonitorRepository;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MonitorMetricsConfiguration {
 
-    public MonitorMetricsConfiguration(MeterRegistry meterRegistry, MonitorRepository monitorRepository) {
-        Gauge.builder("pingboard.monitors.total", monitorRepository, repo -> repo.count())
-                .description("Total number of configured monitors")
-                .register(meterRegistry);
+    @Bean
+    MeterBinder monitorMetricsBinder(MonitorRepository monitorRepository) {
+        return new MeterBinder() {
+            @Override
+            public void bindTo(MeterRegistry registry) {
+                Gauge.builder("pingboard.monitors.total", monitorRepository, MonitorRepository::count)
+                        .description("Total number of configured monitors")
+                        .register(registry);
 
-        Gauge.builder("pingboard.monitors.active", monitorRepository, repo -> repo.findAllByActiveTrueOrderByIdAsc().size())
-                .description("Number of active monitors")
-                .register(meterRegistry);
+                Gauge.builder("pingboard.monitors.active", monitorRepository, repo -> repo.findAllByActiveTrueOrderByIdAsc().size())
+                        .description("Number of active monitors")
+                        .register(registry);
 
-        Gauge.builder("pingboard.monitors.up", monitorRepository, repo ->
-                        repo.findAll().stream().filter(monitor -> monitor.getStatus() == MonitorStatus.UP).count())
-                .description("Number of monitors currently UP")
-                .register(meterRegistry);
+                Gauge.builder("pingboard.monitors.up", monitorRepository, repo ->
+                                repo.findAll().stream().filter(monitor -> monitor.getStatus() == MonitorStatus.UP).count())
+                        .description("Number of monitors currently UP")
+                        .register(registry);
 
-        Gauge.builder("pingboard.monitors.down", monitorRepository, repo ->
-                        repo.findAll().stream().filter(monitor -> monitor.getStatus() == MonitorStatus.DOWN).count())
-                .description("Number of monitors currently DOWN")
-                .register(meterRegistry);
+                Gauge.builder("pingboard.monitors.down", monitorRepository, repo ->
+                                repo.findAll().stream().filter(monitor -> monitor.getStatus() == MonitorStatus.DOWN).count())
+                        .description("Number of monitors currently DOWN")
+                        .register(registry);
+            }
+        };
     }
 }
